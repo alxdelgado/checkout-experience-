042ee29caddf3c2f9cbb34f4fe5efdfcc5f8163b
Alex Delgado <delgadoalex@icloud.com>
2020-04-29 17:44:37 -0500

    Subscription page header build - working on subscription options components

--- src/pages/subscription-page/subscription-page.component.jsx
+++ src/pages/subscription-page/subscription-page.component.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
-import '../subscription-page/subscription-page.styles';
+// import styles
+import {
+    SubscriptionWrapper,
+    SubscriptionTitle,
+    SubscriptionSpan
+} from '../subscription-page/subscription-page.styles';
 
 export default function SubscriptionPage() {
     return (
-        <div className='subscription-wrapper'>
-            <h4>Get Started Today</h4>
-            <span>Choose the right plan and start creating projects</span>
-            <div className='subscription-card'>
-
-            </div>
-        </div>
+        <React.Fragment>
+            <SubscriptionWrapper>
+                <SubscriptionTitle>Get Started Today</SubscriptionTitle>
+                <SubscriptionSpan>Choose the right plan and start creating projects</SubscriptionSpan>
+            </SubscriptionWrapper>
+        </React.Fragment>
     )
-}
+}; 
---
+++ src/pages/subscription-page/subscription-page.styles.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const SubscriptionWrapper = styled.div`
+    box-sizing: border-box; 
+    width: 100%; 
+    height: 100%; 
+    border: 1px solid blue;
+    padding: 0px 0px 10px 10px;  
+`;
+
+export const SubscriptionTitle = styled.h4`
+    text-align: center; 
+    font-size: 20px;
+    padding: 0 auto; 
+`;
+
+export const SubscriptionSpan = styled.p`
+    text-align: center;
+    padding: 0 auto;
+`; 
